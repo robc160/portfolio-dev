a21b15835a88b820a0cecdff0f62b0bc7fbddff7
Rob Culbertson <rsculbertson@gmail.com>
2018-08-01 20:12:53 -0500

    menu stuff

---
+++ dist/js/main.js
@@ -0,0 +1,28 @@
+//SELECT ITEMS
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBrand = document.querySelector(".menu-branding");
+const menuNavItems = document.querySelectorAll(".nav-item");
+const menuBtn = document.querySelector(".menu-btn");
+
+//SET INITIAL STATE OF MENU
+let showMenu = false;
+menuBtn.addEventListener("click", toggelMenu);
+
+function toggelMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBrand.classList.add("show");
+    menuNavItems.forEach(item => item.classList.add("show"));
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBrand.classList.remove("show");
+    menuNavItems.forEach(item => item.classList.remove("show"));
+    showMenu = false;
+  }
+}
